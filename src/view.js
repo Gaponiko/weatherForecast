5a8118df01db9b10a31195892dcb0aa37194cea7
Gaponiko <44677858+Gaponiko@users.noreply.github.com>
2018-11-23 19:32:00 +0300

    Update view.js
    Adding additional functions

--- src/view.js
+++ src/view.js
@@ -1,5 +1,27 @@
 let isInfoBlockVisible   = false;
 
+function showWeather(response) {
+    if(!isInfoBlockVisible) {
+        fillInfo(response);
+        showInfoBlock();
+    }
+    else {
+        infoTextChange(response);
+    }
+}
+
+function fillInfo(response) {
+    citySpan.innerText           = response.name;
+    temperatureSpan.innerText    = Math.round(response.main.temp - 273.15) + '°C';
+    weatherSpan.innerText        = response.weather[0].main;
+    weatherDetailsSpan.innerText = '(' + response.weather[0].description + ')';
+    humiditySpan.innerText       = response.main.humidity + '%';
+    pressureSpan.innerText       = response.main.pressure + ' hPa';
+    cloudsSpan.innerText         = response.clouds.all + '%';
+    coordsSpan.innerText         = '(' + response.coord.lat + ';' + response.coord.lon + ')';
+    windSpeedSpan.innerText      = response.wind.speed + ' m/s'
+}
+
 function startLoader() {
 
     loader.classList.add('round-animation');
